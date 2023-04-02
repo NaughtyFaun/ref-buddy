bfceb2ef73d9ed4ea8c17614a04072cb00177b8d
NaughtyFaun <futa.faun@gmail.com>
2023-04-02 18:37:11 +0300

    tuned gallery overview page

--- image_metadata_overview.py
+++ image_metadata_overview.py
@@ -1,5 +1,6 @@
 from image_metadata import ImageMetadata
 import sqlite3
+import os
 
 
 # def drop_filename(path):
@@ -18,30 +19,22 @@ class ImageMetadataOverview:
 
         rows = c.fetchall()
         images = [ImageMetadata.get_by_id(conn, row[0]) for row in rows]
-        return images
-
-        # qPaths = f"""
-        # SELECT drop_filename(path) AS set_path, COUNT(*) AS image_count
-        # FROM image_metadata
-        # GROUP BY set_path
-        # ORDER BY set_path
-        # """
-        # c.execute(qPaths)
-        # rows = c.fetchall()
-        # paths = [OverviewPath(path=row[0], count=row[1], image_id=ImageMetadata.get_id_by_path(conn, row[0])) for row in rows]
-        # images = [ImageMetadata.get_by_id(conn, p.image_id) for p in paths]
-        #
-        # return paths, images
+        return [OverviewPath.from_image_metadata(img) for img in images]
 
 
 class OverviewPath:
-    def __init__(self, path, count=0, image_id=-1):
+    def __init__(self, path: str, study_type: str, count=0, image_id=-1):
         self.count = count
-        self.path  = path
+        self.path  = os.path.dirname(path)[len(study_type)+1:]
+        self.study_type = study_type
         self.image_id = image_id
 
     def __str__(self):
         return f'[path:{self.path} count:{self.count} id:{self.image_id}]'
+
+    @staticmethod
+    def from_image_metadata(img: 'ImageMetadata'):
+        return OverviewPath(path=img.path, study_type=img.study_type, image_id=img.image_id)
 
 
 if __name__ == "__main__":
