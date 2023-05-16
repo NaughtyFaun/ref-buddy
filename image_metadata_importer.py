f8ec27946f24aa4eb305af29f5fc41a608d98fa4
NaughtyFaun <futa.faun@gmail.com>
2023-05-16 17:35:50 +0300

    cosmetic changes to progress print during images import

--- image_metadata_importer.py
+++ image_metadata_importer.py
@@ -22,27 +22,40 @@ class ImageMetadataImporter:
             count = 0
             max_count = len(filenames)
             msg_dir = f"Importing '{dir_path}'..."
-            print(f"\r{msg_dir}. {len(filenames)} files in this folder", end='')
+            self.print_total(msg_dir, len(filenames))
             for file_name in filenames:
-                if file_name.endswith(formats):
-                    file_path = os.path.join(dir_path, file_name)
-                    file_path = os.path.relpath(file_path, folder_path)
-                    try:
-                        existing_metadata = ImageMetadata.get_by_path(self.conn, file_path)
-                        if not existing_metadata:
-                            count += 1
-                            new_count += 1
-                            print(f"\r{msg_dir} ({count}/{max_count}) New '{file_name}'", end='')
-                            ImageMetadata.create(self.conn, file_path, sts)
-                        else:
-                            count += 1
-                            print(f"\r{msg_dir} ({count}/{max_count}) File exists '{file_name}'", end='')
-                    except Exception as e:
-                        print(f"Error processing  {file_path}: {sys.exc_info()[0]}")
-                        raise
+                if not file_name.endswith(formats):
+                    continue
+                file_path = os.path.join(dir_path, file_name)
+                file_path = os.path.relpath(file_path, folder_path)
+                try:
+                    existing_metadata = ImageMetadata.get_by_path(self.conn, file_path)
+                    if not existing_metadata:
+                        count += 1
+                        new_count += 1
+                        self.print_progress(msg_dir, file_name, count, max_count, True)
+                        ImageMetadata.create(self.conn, file_path, sts)
+                    else:
+                        count += 1
+                        self.print_progress(msg_dir, file_name, count, max_count, False)
+                except Exception as e:
+                    print(f"Error processing  {file_path}: {sys.exc_info()[0]}")
+                    raise
             print(f"")
 
         print(f"\nImport completed! Found {new_count} new files.")
+
+    @staticmethod
+    def print_total(msg_dir, total: int):
+        print(f"\r{msg_dir}. {total} files in this folder", end='')
+
+    @staticmethod
+    def print_progress(msg_dir, file_name: str, cur: int, total: int, is_new: bool):
+        if is_new:
+            print(f"\r{msg_dir} ({cur}/{total}) New '{file_name}'", end='')
+        else:
+            pass
+            # print(f"\r{msg_dir} ({cur}/{total}) File exists '{file_name}'", end='')
 
 
 if __name__ == '__main__':
