5454623c4a54f6bab094f88641f498b2856cf57e
NaughtyFaun <futa.faun@gmail.com>
2023-06-11 16:28:42 +0300

    moved /tagged view into server_ext_tags.py

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template_string, request, send_file, abort, send_from_directory, render_template
-from markupsafe import Markup
 
 from image_metadata_controller import ImageMetadataController as ImageMetadataCtrl
 from image_metadata_overview import ImageMetadataOverview, OverviewPath
@@ -37,30 +36,6 @@ def view_folder(path_id):
     overview = OverviewPath.from_image_metadata(images[0])
 
     return render_template('tpl_view_folder.html', title='Folder', images=images, overview=overview, tags=ImageMetadataCtrl.get_all_tags(sort_by_name=True))
-
-@app.route('/tagged')
-def view_tags():
-    args = request.args
-    tags_all = args.get('tags').split(',')
-    tags_pos = [tag for tag in tags_all if not tag.startswith('-')]
-    tags_neg = [tag[1:] for tag in tags_all if tag.startswith('-')]
-
-    tags_pos = ImageMetadataCtrl.get_tags_by_names(tags_pos)
-    tags_neg = ImageMetadataCtrl.get_tags_by_names(tags_neg)
-
-    page_str = args.get('page', default='1')
-    page = max(int(page_str) - 1, 0)
-
-    limit = 100
-    offset = limit * page
-    response, images = ImageMetadataCtrl.get_all_by_tags(tags_pos, tags_neg, limit, offset)
-
-    overview = {}
-    overview["study_type"] = ', '.join(tags_all)
-    overview["path"] = ""
-
-    panel = Markup(render_template('tpl_tags_panel.html', tags=ImageMetadataCtrl.get_all_tags()))
-    return render_template('tpl_view_folder.html', title='Tags', images=images, overview=overview, panel=panel)
 
 @app.route('/thumb/<path:path>')
 def send_static_thumb(path):
--- server_ext_tags.py
+++ server_ext_tags.py
@@ -1,9 +1,36 @@
-from flask import Blueprint, request, abort, render_template_string
-from image_metadata_controller import ImageMetadataController
+from flask import Blueprint, request, abort, render_template_string, render_template
+from markupsafe import Markup
+
+from image_metadata_controller import ImageMetadataController as Ctrl
 
 
 routes_tags = Blueprint('routes_tags', __name__)
 
+
+@routes_tags.route('/tagged')
+def view_tags():
+    args = request.args
+    tags_all = args.get('tags', default="").split(',')
+    tags_pos = [tag for tag in tags_all if not tag.startswith('-')]
+    tags_neg = [tag[1:] for tag in tags_all if tag.startswith('-')]
+
+    tags_pos = Ctrl.get_tags_by_names(tags_pos)
+    tags_neg = Ctrl.get_tags_by_names(tags_neg)
+
+    page_str = args.get('page', default='1')
+    page = max(int(page_str) - 1, 0)
+
+    limit = 100
+    offset = limit * page
+    response, images = Ctrl.get_all_by_tags(tags_pos, tags_neg, limit, offset)
+
+    overview = {}
+    overview["study_type"] = ', '.join(tags_all)
+    overview["path"] = ""
+
+    tags_available = Ctrl.get_all_tags(sort_by_name=True)
+    panel = Markup(render_template('tpl_tags_panel.html', tags=tags_available))
+    return render_template('tpl_view_folder.html', title='Tags', images=images, overview=overview, panel=panel, tags=tags_available)
 
 @routes_tags.route('/add-image-tags')
 def add_image_rating():
@@ -14,7 +41,7 @@ def add_image_rating():
     if tags is [] or image_ids is []:
         abort(404, 'Empty tags or image ids')
 
-    r = ImageMetadataController.add_image_tags(image_ids, tags)
+    r = Ctrl.add_image_tags(image_ids, tags)
     if not r:
         abort(404, 'Something went wrong, fav not set, probably...')
     return render_template_string(str(r))
