d6cf262def4687ba1a5751022ffba0500a54a16d
NaughtyFaun <futa.faun@gmail.com>
2023-06-17 20:19:25 +0300

    added visual success/fail to rating buttons

--- static/rating.js
+++ static/rating.js
@@ -8,22 +8,28 @@ class RateSingle
     up
     down
     total
-    loadingStyle
+    loadStyle
+    succStyle
+    failStyle
 
     /**
      * @param imageId db image id
      * @param totalId label that shows current rating
      * @param upId rate up button
      * @param downId rate down button
-     * @param loadingStyle
+     * @param loadStyle
+     * @param succStyle
+     * @param failStyle
      */
-    constructor(imageId, totalId, upId, downId, loadingStyle)
+    constructor(imageId, totalId, upId, downId, loadStyle, succStyle, failStyle)
     {
         this.imageId = imageId
         this.up    = document.getElementById(upId)
         this.down  = document.getElementById(downId)
         this.total = document.getElementById(totalId)
-        this.loadingStyle = loadingStyle
+        this.loadStyle = loadStyle
+        this.succStyle = succStyle
+        this.failStyle = failStyle
 
         this.up.addEventListener('click',   () => { this.rate(1) })
         this.down.addEventListener('click', () => { this.rate(-1) })
@@ -34,32 +40,36 @@ class RateSingle
         this.up.setAttribute('disabled', 'true')
         this.down.setAttribute('disabled', 'true')
 
-        if (rating > 0)
-        {
-            this.up.classList.add(this.loadingStyle)
-        } else
-        {
-            this.down.classList.add(this.loadingStyle)
-        }
+        this.up.classList.remove(this.loadStyle, this.succStyle, this.failStyle)
+        this.down.classList.remove(this.loadStyle, this.succStyle, this.failStyle)
+
+        const btn = rating > 0 ? this.up : this.down
+        btn.classList.add(this.loadStyle)
 
         fetch(`/add-image-rating?image-id=${this.imageId}&rating=${rating}`)
             .then(response =>
             {
                 if (!response.ok) throw new Error('Network response was not ok');
+                return response.text()
+            })
+            .then(data =>
+            {
+                this.total.innerText = data
 
-                response.text().then(data =>
-                {
-                    this.total.innerText = data
-                })
+                const btn = rating > 0 ? this.up : this.down
+                btn.classList.add(this.succStyle)
             })
             .catch(error =>
             {
                 console.error('There was a problem with the fetch operation:', error);
+
+                const btn = rating > 0 ? this.up : this.down
+                btn.classList.add(this.failStyle)
             })
             .finally(() =>
             {
-                this.up.classList.remove(this.loadingStyle)
-                this.down.classList.remove(this.loadingStyle)
+                this.up.classList.remove(this.loadStyle)
+                this.down.classList.remove(this.loadStyle)
 
                 this.up.removeAttribute('disabled')
                 this.down.removeAttribute('disabled')
@@ -78,22 +88,28 @@ class RateFolder
     up
     down
     result
-    loadingStyle
+    loadStyle
+    succStyle
+    failStyle
 
     /**
      * @param imageId db image id
      * @param resultId label that shows operation result
      * @param upId rate up button
      * @param downId rate down button
-     * @param loadingStyle
+     * @param loadStyle
+     * @param succStyle
+     * @param failStyle
      */
-    constructor(imageId, resultId, upId, downId, loadingStyle)
+    constructor(imageId, resultId, upId, downId, loadStyle, succStyle, failStyle)
     {
         this.imageId = imageId
         this.up    = document.getElementById(upId)
         this.down  = document.getElementById(downId)
         this.result = document.getElementById(resultId)
-        this.loadingStyle = loadingStyle
+        this.loadStyle = loadStyle
+        this.succStyle = succStyle
+        this.failStyle = failStyle
 
         this.up.addEventListener('click',   () => { this.rate(1) })
         this.down.addEventListener('click', () => { this.rate(-1) })
@@ -104,14 +120,11 @@ class RateFolder
         this.up.setAttribute('disabled', 'true')
         this.down.setAttribute('disabled', 'true')
 
-        if (rating > 0)
-        {
-            this.up.classList.add(this.loadingStyle)
-        }
-        else
-        {
-            this.down.classList.add(this.loadingStyle)
-        }
+        this.up.classList.remove(this.loadStyle, this.succStyle, this.failStyle)
+        this.down.classList.remove(this.loadStyle, this.succStyle, this.failStyle)
+
+        const btn = rating > 0 ? this.up : this.down
+        btn.classList.add(this.loadStyle)
 
         fetch(`/add-folder-rating?image-id=${this.imageId}&rating=${rating}`)
             .then(response =>
@@ -122,15 +135,20 @@ class RateFolder
             .then(data =>
             {
                 this.result.innerText = data
+                const btn = rating > 0 ? this.up : this.down
+                btn.classList.add(this.succStyle)
             })
             .catch(error =>
             {
-                console.error('There was a problem with the fetch operation:', error);
+                console.error('There was a problem with the fetch operation:', error)
+
+                const btn = rating > 0 ? this.up : this.down
+                btn.classList.add(this.failStyle)
             })
             .finally(() =>
             {
-                this.up.classList.remove(this.loadingStyle)
-                this.down.classList.remove(this.loadingStyle)
+                this.up.classList.remove(this.loadStyle)
+                this.down.classList.remove(this.loadStyle)
 
                 this.up.removeAttribute('disabled')
                 this.down.removeAttribute('disabled')
