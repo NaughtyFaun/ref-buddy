e9acf60d76807366620d53255e9a584ce9665838
NaughtyFaun <futa.faun@gmail.com>
2024-01-17 00:36:40 +0300

    refactor: continue to cleanup. Renamed bunch of urls. fixed bulk fetch tags method

--- server_ext_folder.py
+++ server_ext_folder.py
@@ -1,7 +1,57 @@
-from flask import Blueprint, request, abort, render_template_string
+import os
+
+from flask import Blueprint, request, abort, render_template_string, render_template
 from models.models_lump import Session, ImageMetadata, Path
+from server_args_helpers import get_current_paging, get_arg, Args
+from image_metadata_controller import ImageMetadataController as Ctrl
+from server_widget_helpers import get_paging_widget
 
 routes_folder = Blueprint('routes_folder', __name__)
+
+@routes_folder.route('/all')
+def view_tags():
+    page, offset, limit = get_current_paging(request.args)
+    tag_set_id = get_arg(request.args, Args.tag_set)
+    tags_pos, tags_neg = get_arg(request.args, Args.tags)
+
+    session = Session()
+    tags_pos, tags_neg = Ctrl.get_tags_by_set(tag_set_id, tags_pos, tags_neg, session=session)
+    tags_pos_names = Ctrl.get_tag_names(tags_pos, session=session)
+    tags_neg_names = Ctrl.get_tag_names(tags_neg, session=session)
+
+    response, images = Ctrl.get_all_by_tags(tags_pos, tags_neg, limit, offset, session=session)
+
+    overview = {}
+    overview["study_type"] = ', '.join(tags_pos_names)
+    if len(tags_neg) > 0:
+        overview["study_type"] += ' exclude:' + ', '.join(tags_neg_names)
+    overview["path"] = ""
+
+    paging = get_paging_widget(page)
+
+    out = render_template('tpl_view_folder.html', title='Tags', images=images, overview=overview, paging=paging)
+    session.close()
+    return out
+
+@routes_folder.route('/folder/<int:path_id>')
+def view_folder(path_id):
+    rating = get_arg(request.args, Args.min_rating)
+    tags_pos, tags_neg = get_arg(request.args, Args.tags)
+    tag_set_id = get_arg(request.args, Args.tag_set)
+
+    session = Session()
+    tags_pos, tags_neg = Ctrl.get_tags_by_set(tag_set_id, tags_pos, tags_neg, session=session)
+
+    study_type, path, images = Ctrl.get_all_by_path_id(path_id, tags=(tags_pos,tags_neg), min_rating=rating, session=session)
+    if len(images) == 0:
+        overview = None
+    else:
+        overview = images[0]
+        overview.path_dir = os.path.dirname(overview.path_abs)
+
+    out = render_template('tpl_view_folder.html', title='Folder', images=images, overview=overview)
+    session.close()
+    return out
 
 @routes_folder.route('/folder-ord-add')
 def add_folder_ord():
--- server_widget_helpers.py
+++ server_widget_helpers.py
@@ -1,9 +1,7 @@
-from image_metadata_controller import ImageMetadataController as Ctrl
-
 from flask import render_template
 from markupsafe import Markup
 
-from models.models_lump import TagSet, Board, Session
+from models.models_lump import Board, Session
 
 
 def get_paging_widget(selected_page:int=0, span:int=3, skip=10) -> 'str':
@@ -16,26 +14,6 @@ def get_paging_widget(selected_page:int=0, span:int=3, skip=10) -> 'str':
     start = max(selected_page - span, 0) + 1
     count = span * 2 + 1 + start
     return Markup(render_template('tpl_widget_paging.html', pages=range(start, count), start_page=selected_page+1, skip=skip))
-
-def get_tags_editor(tags=None, session=None):
-    if tags is None:
-        tags = Ctrl.get_all_tags(session=session)
-    else:
-        tags = tags[:]
-
-    tags = sorted(tags, key=lambda t: (t.color_id, t.tag))
-    return Markup(render_template('tpl_widget_tags_editor_panel.html', tags=tags))
-
-def get_tags_filter(tags=None, session=None):
-    if tags is None:
-        tags = Ctrl.get_all_tags(session=session)
-    else:
-        tags = tags[:]
-
-    tagsets = session.query(TagSet).all()
-
-    tags = sorted(tags, key=lambda t: (t.color_id, t.tag))
-    return Markup(render_template('tpl_widget_tags_filter_panel.html', tags=tags, tagsets=tagsets))
 
 def get_boards_all(session=None):
     auto_close = False
