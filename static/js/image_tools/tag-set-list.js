ee282483867c8293686838f92cb6a9910d29a856
NaughtyFaun <futa.faun@gmail.com>
2024-02-04 04:52:39 +0300

    refactor: TagSetList now updates get search param

--- static/js/image_tools/tag-set-list.js
+++ static/js/image_tools/tag-set-list.js
@@ -1,10 +1,11 @@
 import {ApiTags} from 'api'
+import {UrlWrapper} from '/static/js/main.js'
 
 class TagSetList
 {
     list
 
-    constructor(selList)
+    constructor(selList, selectValue = null)
     {
         this.list = document.querySelector(selList)
 
@@ -20,13 +21,43 @@ class TagSetList
                 })
 
                 this.list.remove(0)
-                this.list.selectedIndex = 0
+
+                const url = new UrlWrapper(window.location.href)
+                this.value = url.getSearch('tag-set')
             })
+
+        this.list.addEventListener('change', (e) => { this.onChange(e) })
+    }
+
+    onChange(e)
+    {
+        const newVal = this.value
+        const url = new UrlWrapper(window.location.href)
+        const oldVal = url.getSearch('tag-set')
+
+        if (oldVal === newVal) return
+
+        url.setSearch('tag-set', newVal)
+        url.updateLocationHref()
+    }
+
+    set value(val)
+    {
+        let idx = Array.from(this.list.options).findIndex(o => o.value === val)
+        if (idx === this.list.selectedIndex) return
+
+        if (idx === -1) idx = 0
+        this.list.selectedIndex = idx
     }
 
     get value()
     {
-        return this.list.value
+        return this.getValueByIndex(this.list.selectedIndex)
+    }
+
+    getValueByIndex(idx)
+    {
+        return this.list.options[idx].value
     }
 }
 
