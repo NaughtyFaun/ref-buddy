a41206686ef4af7e2e24ab876241ff5a4c5b05cd
NaughtyFaun <futa.faun@gmail.com>
2026-03-10 14:01:08 +0300

    fix: terminating launcher also terminates server app

--- app/__main__.py
+++ app/__main__.py
@@ -2,4 +2,4 @@ import uvicorn
 
 from shared_utils.Env import Env
 
-uvicorn.run('app.here_you_go_uvicorn:asgi_app', host='0.0.0.0', port=int(Env.SERVER_PORT), reload=True, lifespan="off")
+uvicorn.run('app.here_you_go_uvicorn:asgi_app', host='0.0.0.0', port=int(Env.SERVER_PORT), reload=False, lifespan="off")
