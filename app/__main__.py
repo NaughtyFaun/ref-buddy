784726b33b873bc381606285f2915461fca28453
NaughtyFaun <futa.faun@gmail.com>
2026-03-13 13:33:54 +0300

    refactor: when attempt to debug code, run quart without uvicorn

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,10 @@
 import uvicorn
 
+from app.utils.misc import is_debugging
 from shared_utils.Env import Env
 
-uvicorn.run('app.here_you_go_uvicorn:asgi_app', host='0.0.0.0', port=int(Env.SERVER_PORT), reload=False, lifespan="off")
+if is_debugging():
+    from app import app_quart
+    app_quart.run(host='0.0.0.0', port=int(Env.SERVER_PORT))
+else:
+    uvicorn.run('app.here_you_go_uvicorn:asgi_app', host='0.0.0.0', port=int(Env.SERVER_PORT), reload=False, lifespan="off")
