c97abbf4722c2cab5344b7584fb40e0baafe0de8
NaughtyFaun <futa.faun@gmail.com>
2026-03-16 18:05:37 +0300

    test: basic tests for mp4 and gifs routes; fixture data for gif and mp4

--- tests/test_app/fixtures/data.py
+++ tests/test_app/fixtures/data.py
@@ -2,7 +2,7 @@ from app.models.models_lump import Path, ImageMetadata
 
 
 def add_4_images_1_path(session) -> None:
-    session.add(Path(id=1, path="everything"))
+    session.merge(Path(id=1, path="everything"))
     session.flush()
 
     new_image = lambda s, imid, fn: s.add(ImageMetadata(image_id=imid,
@@ -14,4 +14,26 @@ def add_4_images_1_path(session) -> None:
     new_image(session, 2, "Lenna_2.jpg")
     new_image(session, 3, "Lenna_3.jpg")
     new_image(session, 4, "Lenna_4.jpg")
+    session.commit()
+
+def add_1_mp4_1_path(session) -> None:
+    session.merge(Path(id=1, path="everything"))
+    session.flush()
+
+    session.add(ImageMetadata(image_id=5,
+                              filename='Lenna_5_source_mp4.mp4.gif',
+                              path_id=1,
+                              source_type_id=3,
+                              category_id=1))
+    session.commit()
+
+def add_1_gif_1_path(session) -> None:
+    session.merge(Path(id=1, path="everything"))
+    session.flush()
+
+    session.add(ImageMetadata(image_id=6,
+                              filename='Lenna_6_source_gif.gif',
+                              path_id=1,
+                              source_type_id=2,
+                              category_id=1))
     session.commit()
---
+++ tests/test_app/routes/test_image_single_video_gif_base.py
@@ -0,0 +1,63 @@
+import pytest
+
+from tests.test_app.fixtures.data import add_4_images_1_path, add_1_mp4_1_path, add_1_gif_1_path
+
+
+@pytest.fixture(scope="module", autouse=True)
+def add_images_for_all_tests(setup_database, session_real):
+    session = session_real()
+    add_4_images_1_path(session)
+    add_1_mp4_1_path(session)
+    add_1_gif_1_path(session)
+    session.close()
+
+@pytest.mark.asyncio
+async def test_image_get_html(client):
+    # video
+    resp = await client.get('/study-image/5')
+    assert resp.status_code == 200
+
+    # gif
+    resp = await client.get('/study-image/6')
+    assert resp.status_code == 200
+
+@pytest.mark.asyncio
+async def test_image_get_image_file(client):
+    resp = await client.get('/video/5') # source mp4
+    assert resp.status_code == 200
+    resp = await client.get('/image/5') # mp4 to gif
+    assert resp.status_code == 200
+
+    resp = await client.get('/image/6')
+    assert resp.status_code == 200
+
+@pytest.mark.asyncio
+async def test_image_get_data(client):
+    resp = await client.get('/image-info/5')
+    assert resp.status_code == 200
+    json = await resp.json
+    assert json['url_image'].startswith('/video')
+    assert json['content_type'] == 3
+
+    resp = await client.get('/image-info/6')
+    assert resp.status_code == 200
+    json = await resp.json
+    assert json['content_type'] == 2
+
+@pytest.mark.asyncio
+async def test_image_get_thumb(client):
+    resp = await client.get('/thumb/5.jpg')
+    assert resp.status_code == 200
+
+    resp = await client.get('/thumb/6.jpg')
+    assert resp.status_code == 200
+
+@pytest.mark.asyncio
+async def test_image_get_anim_frames(client):
+    resp = await client.get('/anim-info/6')
+    assert resp.status_code == 200
+
+@pytest.mark.asyncio
+async def test_image_get_anim_frames(client):
+    resp = await client.get('/anim-frames-zip/6')
+    assert resp.status_code == 200
