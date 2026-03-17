23a1e40876073215a8c9dcca2acb21bc7744b312
NaughtyFaun <futa.faun@gmail.com>
2026-03-17 16:55:30 +0300

    test: cleanup on test session finished; fixtures for tmp environments; some environment loading tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,12 @@
 import os
 
 os.environ['APP_ENV'] = 'test'
+
+def config_tmp_factory(source:str, new_source:str, marker:str) -> str:
+    with open(source, 'r') as f:
+        with open(new_source, 'w') as new_f:
+            lines = []
+            while line := f.readline():
+                lines.append(line.replace('__', marker))
+            new_f.writelines(lines)
+    return new_source
