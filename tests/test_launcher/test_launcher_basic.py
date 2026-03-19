a7681f5164982408c4d7706f4eee3a027143dd90
NaughtyFaun <futa.faun@gmail.com>
2026-03-19 17:13:40 +0300

    fix: skipping tkinter window launch test for now

--- tests/test_launcher/test_launcher_basic.py
+++ tests/test_launcher/test_launcher_basic.py
@@ -1,6 +1,11 @@
+import os
+
+import pytest
+
 from launcher import create_window
 
 
+@pytest.mark.skipif("DISPLAY" not in os.environ, reason="No display")
 def test_launcher_launches(config_path_testing_fresh_func):
     window = create_window(testing=True)
     assert window is not None
