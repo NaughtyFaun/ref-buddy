9e0bffc4fba3647f75b015edb7ff3b0c12b3c56a
NaughtyFaun <futa.faun@gmail.com>
2026-03-19 16:14:54 +0300

    test,fix: updating THUMB_STATIC path on environment change

--- tests/test_app/routes/conftest.py
+++ tests/test_app/routes/conftest.py
@@ -4,10 +4,12 @@ from quart.testing import QuartClient
 from app.models import Session
 
 from app import app_quart
+from shared_utils.env import Env
 
 
 @pytest.fixture
 def client() -> QuartClient:
+    app_quart.config['THUMB_STATIC'] = Env.THUMB_PATH
     return app_quart.test_client()
 
 @pytest.fixture(scope="module")
