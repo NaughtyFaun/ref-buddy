4b382405903c8b733262fec003d109c413eea6ce
NaughtyFaun <futa.faun@gmail.com>
2026-03-26 16:23:06 +0300

    refactor!: route /tags-ai/suck-folder-in validates and converts data with pydantic

--- app/routes/tags_ai.py
+++ app/routes/tags_ai.py
@@ -1,11 +1,11 @@
 import json
 import os
-from datetime import datetime, timezone
+from typing import Annotated
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator, BeforeValidator
 from quart import Blueprint, request, abort, render_template, redirect, jsonify
 
-from app.common.dto_basic import EmptyResponse
+from app.common.dto_basic import EmptyResponse, IntResultResponse
 from app.models import Session
 from app.models.models_lump import Tag, ImageMetadata, TagSet, TagAi, ImageTagAi, TagAiToTag, ImageTag
 from sqlalchemy.sql import exists
@@ -24,6 +24,25 @@ class ExportedUrlRow(BaseModel):
 
 class ExportedUrlsToSave(BaseModel):
     json_urls:list[ExportedUrlRow]
+
+class ImportedAiImageTags(BaseModel):
+    tag_local_id:str
+    rating:int
+
+class ImportedAiImage(BaseModel):
+    id:int
+    timestamp:Annotated[int, BeforeValidator(lambda x: int(x))]
+    tags:list[ImportedAiImageTags]
+
+    @model_validator(mode='before')
+    @classmethod
+    def convert_tags(cls, values):
+        values['tags'] = list(map(lambda key: {'tag_local_id': int(key), 'rating':values['tags'][key]}, values['tags']))
+        return values
+
+class ImportedAiFile(BaseModel):
+    tags:list[str]
+    images:list[ImportedAiImage]
 
 @routes_tags_ai.route('/tags-ai/begin')
 async def begin_processing():
@@ -71,39 +90,39 @@ async def suck_in_all_tags_in_folder():
             with open(os.path.join(path, file), 'r') as f:
                 files_count += 1
 
-                data = json.load(f)
+                data_dict = json.load(f)
+
+                data = ImportedAiFile.model_validate(data_dict)
 
                 # import new tags
-                count_new = 0
-                for tag in data['tags']:
+                new_tags = []
+                for tag in data.tags:
                     query = session.query(exists().where(TagAi.tag == tag))
                     has_tag = session.scalar(query)
                     if has_tag: continue
-                    count_new += 1
-                    session.add(TagAi(tag=tag))
+                    new_tags.append(TagAi(tag=tag))
 
-                if count_new > 0:
+                if len(new_tags) > 0:
+                    session.add_all(new_tags)
                     session.commit()
 
                 # build mapping for internal tag id to db tag id
                 tags_to_db = {}
-                for i in range(len(data['tags'])):
-                    tag_id = session.query(TagAi.id).filter(TagAi.tag == data['tags'][i]).one()[0]
+                for i in range(len(data.tags)):
+                    tag_id = session.query(TagAi.id).filter(TagAi.tag == data.tags[i]).one()[0]
                     tags_to_db[str(i)] = tag_id
 
                 # import image-tag + rating
                 count_new = 0
-                for img in data['images']:
-                    img_id = int(img['id'])
-                    timestamp = datetime.fromtimestamp(int(img['timestamp']), tz=timezone.utc)
-                    for key, value in img['tags'].items():
-                        result = session.merge(ImageTagAi(image_id=img_id, tag_id=tags_to_db[key], rating=value, imported_at=timestamp))
+                for img in data.images:
+                    for t in img.tags:
+                        result = session.merge(ImageTagAi(image_id=img.id, tag_id=tags_to_db[t.tag_local_id], rating=t.rating, imported_at=img.timestamp))
                         if inspect(result).pending:
                             count_new += 1
                 if count_new > 0:
                     session.commit()
 
-    return jsonify({'ok': f'{path}'})
+    return jsonify(IntResultResponse.model_validate(count_new))
 
 @routes_tags_ai.route('/tags-ai/update-mapped-tags')
 async def update_mapped_tags():
